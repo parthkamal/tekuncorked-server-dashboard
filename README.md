40606bc673a3ea51a57d452a56f7d9e19f01a7bc
parthkamal <kamalparth40@gmail.com>
2023-09-28 16:42:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tekuncorked-server-dashboard
+
+
+- this project will be for an iot device to register in the dashboard
+- the user can configure the temperature reading on entering the dashboard
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+
+  // render function
+  render() {
+    return (
+      <div>
+      welcome to the tekuncorked-server-dashboard
+      </div>
+    );
+  }
+}
+
+export default App;
